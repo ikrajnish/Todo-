dd0f1094b63d8dbedc1fdf227fe094f37aeb11a9
ikrajnish <kumarajnish20@gmail.com>
2024-07-22 22:54:11 +0530

    Base of the App

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {createTodo,updateTodo} =require("../types.js")
+const app = express();
+const PORT =3000;
+
+app.use(express.json());
+
+app.post("/todo", function(req,res){
+    const createPayload = req.body;
+    const parsedPayload =req.body;
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
+});
+
+app.get("/todos", function(req,res){
+    
+});
+
+app.put("/completed", function(req,res){
+    
+});
+
+app.listen(PORT);
